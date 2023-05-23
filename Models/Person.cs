b55ed3e44c924e67568ed78dba3a4e5d00b58349
Kirill Dolbilov <dolbilov.kirill@gmail.com>
2023-05-23 13:26:43 +0300

    feat: add Person model

---
+++ Models/Person.cs
@@ -0,0 +1,12 @@
+namespace BasicDataTemplateSample.Models;
+
+public class Person
+{
+    public string? FirstName { get; init; }
+    
+    public string? LastName { get; init; }
+    
+    public int Age { get; init; }
+    
+    public Sex Sex { get; init; }
+}
